48e7c0ecac187562e8fb982bc3ec536add4177a6
Asif <muhammadasifazmi@gmail.com>
2023-03-19 19:57:01 +0530

    Assignment Submission

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+      RWOnly obj=new RWOnly();
+      //obj.name="asif"; //The name variable is private and cannot be accessed outside of its class
+      obj.setter("Asif Azmi");
+      System.out.println(obj.getter());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    public String getter(){
+        return name;
+    }
+    public void setter(String n){
+        name=n;
+    }
 }
