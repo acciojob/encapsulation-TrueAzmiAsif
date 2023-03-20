493c23ba444592940f468a5f01efee06844564bd
Asif <muhammadasifazmi@gmail.com>
2023-03-20 22:00:18 +0530

    Final submission

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main {
   public static void main(String args[]){
       RWOnly obj=new RWOnly();
       //obj.name="asif"; //The name variable is private and cannot be accessed outside of its class
-      obj.setter("Asif Azmi");
-      System.out.println(obj.getter());
+      obj.setName("Asif Azmi");
+      System.out.println(obj.getName());
     }
 }
